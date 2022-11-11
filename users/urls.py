73846028a292ff0b80af8cdef19f6f7230b5e52b
student <jasurabdibayev0@gmail.com>
2022-11-11 14:10:22 +0500

    rest api

---
+++ users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import hello
+
+urlpatterns = [
+    path('hello/', hello),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['POST'])
+def hello(request):
+    return Response('hello')
