c8b410f6762e5097a9611a8c07a602169f09cfc8
student <jasurabdibayev0@gmail.com>
2023-02-21 11:30:02 +0500

    all project

--- Blog-Application/blog/urls.py
+++ Blog-Application/blog/urls.py
@@ -4,14 +4,16 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # post views
-    # path('', views.post_list, name='post_list'),
+# Post views
+    path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail,
+    path('tag/<slug:tag_slug>/',
+         views.post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
          name='post_detail'),
     path('<int:post_id>/share/',
          views.post_share, name='post_share'),
     path('<int:post_id>/comment/',
-         views.post_comment, name='post_comment'),
-#########################################################
+        views.post_comment, name='post_comment'),
 ]
--- Blog-Application/blog/views.py
+++ Blog-Application/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render,get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -5,11 +6,16 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from .models import Post
 from django.http import Http404
+from taggit.models import Tag
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -23,7 +29,9 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                   {'posts': posts})
+                   {'posts': posts,
+                    'tag': tag,
+                    })
 
 
 def post_detail(request, year, month, day, post):
@@ -37,11 +45,20 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
-                   'comments': comments, 'form': form})
+                   'comments': comments,
+                   'form': form,
+                   'similar_posts': similar_posts,
+                   })
 
 def post_share(request, post_id):
 # Retrieve post by id
